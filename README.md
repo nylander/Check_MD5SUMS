a9ab24fbd032bcc36a68e5a9e5d2919262fd7780
nylander <Johan.Nylander@bils.se>
2016-05-19 10:45:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# check_md5sums.sh
+
+## Version
+
+11/20/2015 11:56:55 AM
+
+## By
+
+Johan.Nylander\@bils.se
+
+## Description
+
+Check md5sums on any md5 files found in directory tree (starting in cwd).
+
+Files need to end in `.md5` (case insensitive).
+
+## Options
+
+    -?, -h  show help
+
+    -v      be more verbose
+
---
+++ check_md5sums.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -l
+
+function show_help () {
+cat <<HEREDOC
+File:
+    check_md5sums.sh
+
+Version:
+    11/20/2015 11:56:55 AM
+
+By:
+    Johan.Nylander\@bils.se
+
+Description:
+    Check md5sums on any md5 files found in dir tree.
+    Files need to end in .md5 (case insensitive).
+
+Options:
+    -?, -h  -- show this help
+    -v      -- be more verbose
+
+HEREDOC
+}
+
+if ! hash md5sum 2>/dev/null ; then
+  echo "Error: program md5sum is not found. Aborting"
+  exit 1
+fi
+
+verbose=0
+found=0
+
+while getopts "h?v" opt; do
+  case "$opt" in
+  h|\?)
+    show_help
+    exit 0
+    ;;
+  v)  verbose=1
+    ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+dirs=($(find "$PWD" -type d))
+
+for dir in "${dirs[@]}"; do
+  cd "$dir"
+  myarray=($(find ./ -maxdepth 1 -iname "*.md5"))
+  if [ ${#myarray[@]} -gt 0 ]; then
+    found=1
+    if [ "$verbose" -eq 1 ]; then
+      echo "Checking ${#myarray[@]} md5-sum files in $PWD:"
+      #echo ''
+    fi
+    for f in "${myarray[@]}" ; do
+        md5sum -c $f
+    done
+    if [ "$verbose" -eq 1 ]; then
+      echo ''
+    fi
+  fi
+done
+if [ "$found" -eq 0 ]; then
+  echo "No md5 files found"
+fi
+if [ "$verbose" -eq 1 ]; then
+  echo 'Done with script' "$0"
+fi
+
